1ae058f795efc2d2940afc4d9f17a27402b828c4
徐乐乐 <x2892379404@163.com>
2022-05-15 18:08:04 +0800

    登录逻辑

--- XueJi_Manage/src/main/java/com/fc/controller/AdminController.java
+++ XueJi_Manage/src/main/java/com/fc/controller/AdminController.java
@@ -4,9 +4,15 @@ import com.fc.service.AdminService;
 import com.fc.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("admin")
@@ -14,23 +20,69 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
-    @PostMapping("loginService")
+   /* @PostMapping("loginService")
     public ResultVO login(@RequestParam String userName,
                           @RequestParam String userPw){
 
 
         return adminService.login(userName,userPw);
 
+    }*/
+        @PostMapping("loginService")
+        public ModelAndView login1(@RequestParam String userName,
+                @RequestParam String userPw,
+                Integer remember,
+                HttpSession session,
+                HttpServletResponse response,
+                ModelAndView mv){
+            //在这里写逻辑
+            ResultVO vo = adminService.login(userName, userPw);
+
+            if (vo.getCode()==0){
+                //登录失败
+                mv.addObject("resultInfo",vo);
+
+                mv.setViewName("forward:/login.jsp");
+            }else {
+                //登录成功
+                session.setAttribute("admin",vo.getData());
+
+                Cookie cookie;
+                if (remember!=null&&remember==1){
+                    cookie = new Cookie("JSESSIONID", session.getId());
+
+                    cookie.setMaxAge(30 * 60);
+                }else {
+                    cookie = new Cookie("JSESSIONID", null);
+
+                    cookie.setMaxAge(-1);
+                }
+                response.addCookie(cookie);
+
+                mv.setViewName("forward:/admin/left.jsp");
+            }
+            return mv;
+        }
+        @GetMapping("loginout")
+        public ModelAndView loginOut(HttpSession session,
+                HttpServletResponse response,
+                ModelAndView mv){
+            Cookie cookie = new Cookie("JSESSIONID", null);
+
+            //销毁cookie
+            cookie.setMaxAge(0);
+
+            response.addCookie(cookie);
+
+            session.removeAttribute("admin");
+
+            //销毁session
+            session.invalidate();
+
+            //重定向到登录
+            mv.setViewName("redirect:/login.jsp");
+            return mv;
+        }
     }
-    @PostMapping("loginService1")
-    public String login1(@RequestParam String userName,
-                          @RequestParam String userPw){
-        //在这里写逻辑
 
 
-
-        System.out.println(111111);
-        return "admin/adminMana.jsp";
-
-    }
-}
