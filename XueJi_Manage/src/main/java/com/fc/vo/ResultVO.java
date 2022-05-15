6e03cfc0e094ac9acb29592b24e7882545edbec2
徐乐乐 <x2892379404@163.com>
2022-05-15 14:43:57 +0800

    登录VO更改一致

--- XueJi_Manage/src/main/java/com/fc/vo/ResultVO.java
+++ XueJi_Manage/src/main/java/com/fc/vo/ResultVO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResultVo {
+public class ResultVO {
     Integer code;
     String message;
     Boolean success;
