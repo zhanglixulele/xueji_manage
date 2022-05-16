66c4c8dc8db5bf9b92a01041a8ad8a4484332e86
徐乐乐 <x2892379404@163.com>
2022-05-16 21:55:37 +0800

    合并zxf

--- XueJi_Manage/src/main/java/com/fc/controller/jiangchengController.java
+++ XueJi_Manage/src/main/java/com/fc/controller/jiangchengController.java
@@ -3,10 +3,7 @@ package com.fc.controller;
 import com.fc.entity.TJiangcheng;
 import com.fc.service.JiangChengService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
